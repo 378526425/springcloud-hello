c369d9ef87ee4b2dc106ed3c2a9f1a9e9f8e6884
sc_wanglei <378526425@qq.com>
2021-06-16 17:30:55 +0800

    修改注解

--- src/main/java/com/springcloud/hello/controller/HelloController.java
+++ src/main/java/com/springcloud/hello/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.springcloud.hello.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${server.port}")
+    private String port;
     @GetMapping("/hello")
     public String hello() {
-        return "hello spring cloud";
+        return "hello spring cloud 我是："+this.port;
     }
 }
